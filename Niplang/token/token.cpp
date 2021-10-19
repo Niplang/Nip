ccdb10457971db8d2692cbc9cbe9917106adf730
Pavan D Umesh <pavandumesh2027@gmail.com>
2021-10-19 10:59:33 +0530

    Updated token.cpp

---
+++ Niplang/token/token.cpp
@@ -0,0 +1,9 @@
+#include <string>
+#include <vector>
+
+std::vector<std::string> token (std::string str) {
+    // The vector that returns the lexeme of the given token
+    std::vector<std::string> lexeme;
+
+    return lexeme;
+}
